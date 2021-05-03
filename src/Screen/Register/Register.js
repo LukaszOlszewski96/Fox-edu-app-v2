db5422b50ef0116c59d52b9da2a70ef00ec97ac5
LukaszOlszewski96 <lukaszolszewski96@gmail.com>
2021-05-03 23:16:52 +0200

    Add register screen

--- src/Screen/Register/Register.js
+++ src/Screen/Register/Register.js
@@ -4,35 +4,38 @@ import {HiOutlineMail} from 'react-icons/hi'
 import {RiLockPasswordLine} from 'react-icons/ri'
 import {BsPerson} from 'react-icons/bs'
 import {ImCheckboxUnchecked} from 'react-icons/im'
-import NavBar from '../../Components/NavBar/NavBar'
+import {IoChevronBackCircleOutline} from 'react-icons/io5'
+import {BiRocket} from 'react-icons/bi'
 
 function Register() {
     return (
-        <div className="main-conteiner-register">
-            <div className="top-conteiner"><NavBar/></div>
-            <div className="Register-conteiner">
-                <h2>Dane uczestnika e-szkoleń:</h2>
-                    <form className="input-form-imie">
-                        <div className="icon-imie"><BsPerson/></div>
-                        <input  className="input-text-imie" type="text" placeholder="Imię" name="userName" />
+        <div className="register-conteiner" >
+            <a href="/login" className="back-to-login"><IoChevronBackCircleOutline/></a>
+            <div className="register-box">
+                <p className="title-register">Dane uczestnika e-szkoleń:</p>
+                    <form className="input-form">
+                        <div className="icon-form"><BsPerson/></div>
+                        <input  className="input-text" type="text" placeholder="Imię" name="userName" />
                     </form>
-                    <form className="input-form-nazwisko">
-                        <div className="icon-nazwisko"><BsPerson/></div>
-                        <input  className="input-text-nazwisko" type="text" placeholder="Nazwisko" name="userSurname" />
+                    <form className="input-form">
+                        <div className="icon-form"><BsPerson/></div>
+                        <input  className="input-text" type="text" placeholder="Nazwisko" name="userSurname" />
                     </form>
-                    <form className="input-form-email">
-                        <div className="icon-password"><HiOutlineMail/></div>
-                        <input className="input-text-email" type="text" placeholder="E-mail" name="userEmail" />
+                    <form className="input-form">
+                        <div className="icon-form"><BiRocket/></div>
+                        <input  className="input-text" type="text" placeholder="Nazwa konta" name="userSurname" />
                     </form>
-                    <form className="input-form-password">
-                        <div className="icon-password"><RiLockPasswordLine/></div>
-                        <input  className="input-text-password" type="password" placeholder="Hasło" name="userPassword" />
+                    <form className="input-form">
+                        <div className="icon-form"><HiOutlineMail/></div>
+                        <input className="input-text" type="text" placeholder="E-mail" name="userEmail" />
                     </form>
-                    <a className="subscription" href="/"><ImCheckboxUnchecked/> Chcę otrzymywać wiadomości e-mail z ofertami.</a>
-                    <button className="Button-Register" >
-                    Zarejestruj się
-                    </button>
-                    <p className="text-register">Rejestracja oznacza akceptację<a href="/">Warunków użytkowania</a><b/> i <a href="/">Polityki</a> w zakresie<a href="/"> Ochrony prywatności</a>. </p>
+                    <form className="input-form">
+                        <div className="icon-form"><RiLockPasswordLine/></div>
+                        <input  className="input-text" type="password" placeholder="Hasło" name="userPassword" />
+                    </form>
+                    <a className="subscription active" href="/"><ImCheckboxUnchecked/> Chcę otrzymywać wiadomości e-mail z ofertami.</a>
+                    <button className="button-register" >Zarejestruj się</button>
+                    <p className="political">Rejestracja oznacza akceptację<a href="/"> Warunków użytkowania </a><br/> i <a href="/">Polityki</a> w zakresie<a href="/"> Ochrony prywatności</a>. </p>
             </div>
         </div>
     )
