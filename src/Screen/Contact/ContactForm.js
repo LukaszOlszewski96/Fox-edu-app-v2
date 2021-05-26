c905a1024a8d16484132f7ea7a4a5ef3485b01fb
LukaszOlszewski96 <lukaszolszewski96@gmail.com>
2021-05-27 00:15:26 +0200

    Add contact form

--- src/Screen/Contact/ContactForm.js
+++ src/Screen/Contact/ContactForm.js
@@ -11,15 +11,9 @@ function ContactForm() {
 
     return (
         <div className="contact-form-conteiner">
-            <div className="contact-form-bottom-box">
             <a href="/" className="exit-contact"><AiFillCloseSquare/></a>
-            <img className="send_image" src="image/send-image.png"/>
-            <div className="contact-form-box">
-                <div className="text-contact-box">
                     <p className="title-contact-form">Get in touch</p>
                     <p className="description-contact-form">Feel free to get in touch with me. I am always open to discussing new projects, creative ideas or opportunities to be part of your visions.</p>
-                </div>
-                <img alt="contact" src="image/contact_image.png"/>
                 <div className="input-box-contact">
                     <form className="name-input-form">
                         <div className="icon"><IoPersonCircleOutline/></div>
@@ -39,8 +33,6 @@ function ContactForm() {
                     <button href="/" type="submit" className="button-submit">Submit</button>
                 </div>
                     </div>
-                </div>
-            </div>
     )
 }
 
